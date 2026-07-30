8d480a91158ecb5eb956d2dbbcb085209f86a17c
Cherenet Degefe <cheramlak21@gmail.com>
2026-07-30 13:32:59 +0000

    files removed

--- src/lib/pdfWorker.ts
+++
@@ -1,4 +0,0 @@
-import { pdfjs } from "react-pdf";
-
-// Use CDN worker matching installed pdfjs-dist version
-pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.mjs`;
--- src/pages/Index.tsx
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom";
-import { Button } from "@/components/ui/button";
-import { Beaker } from "lucide-react";
-
-const Index = () => {
-  return null;
-};
-
-export default Index;
