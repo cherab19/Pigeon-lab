2971ab58565e4bea3d426c0cd68d73abe5b0b098
Cherenet Degefe <cheramlak21@gmail.com>
2026-09-09 09:40:55 +0000

    updated

--- elab/next.config.mjs
+++ elab/next.config.mjs
@@ -14,4 +14,7 @@ const nextConfig = {
     cpus: 1,
   },
 };
-export default withPWA({ dest: "public", disable: process.env.NODE_ENV === "development" })(nextConfig);
+export default withPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development" || process.env.DISABLE_PWA === "1",
+})(nextConfig);
